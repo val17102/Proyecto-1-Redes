208910f0c7d010400ba1ef6ffd1993a28d515b61
DavidSoto5317 <urielsonic@gmail.com>
2020-08-16 01:33:54 -0600

    Se implementa lo logica para hacer el set del juego en la partida ya dentro del servidor con el protocolo

--- mainServer.py
+++ mainServer.py
@@ -1,10 +1,30 @@
-import socket, os, pickle, threading
+import socket, os, pickle, threading, random
 from operator import itemgetter 
+
+### Variables para el manejo de salas y conexiones de clientes
 HOST = 'localhost'
 PORT = 50001
 ROOMS = {}
 ROOMScon = {}
 startGame = {}
+ROOMpiles = {}
+ROOMamountDefuses = {}
+ROOMpilesPlayers = {}
+ROOMSturnPlayers = {}
+ROOMSplayersAlive = {}
+
+### Variables para el manejo de una partida en una sala
+CARDNO = 1
+CARDATACK = 2
+CARDSKIP = 3
+CARDFAVOR = 4
+CARDSHUFFLE = 5
+CARDSEEFUTURE = 6
+CARDDEFUSE = 7
+CARDBOMB = 8
+
+### Funciones para la creacion, manejor y control de ROOMS
+## Crea el thread cuando se quiere hacer una sala
 def thread_function1(port):
     banner = 0
     RPORT = port
@@ -13,7 +33,12 @@ def thread_function1(port):
     r.bind((HOST, RPORT))
     print('starting to listen in Room', RPORT)
     ROOMScon[port] = list()
+    ROOMSplayersAlive[port] = list()
+    ROOMpilesPlayers[port] = {}
     startGame[port] = False
+    ROOMpiles[port] = [1,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,6]
+    ROOMamountDefuses[port] = 6
+    ROOMSturnPlayers[port] = list()
     r.listen(5)
     roomThreads = list()
     while (ROOMS[RPORT] < 5):
@@ -21,7 +46,9 @@ def thread_function1(port):
         roomCon, temp = r.accept()
         username = "User "+str(ROOMS[RPORT])
         ROOMScon[RPORT].append(roomCon)
+        ROOMSplayersAlive[RPORT].append(roomCon)
         ROOMS[RPORT] = ROOMS[RPORT] + 1
+        ROOMpilesPlayers[RPORT][roomCon] = []
         print ('Connected by ', temp, ' on room', RPORT)
         print(ROOMS)
         thread_client = threading.Thread(target = game, args=[roomCon,RPORT,username])
@@ -36,10 +63,53 @@ def game(cli_sock, port, username):
         print(username, " has sent a message")
         objeto = pickle.loads(responseP)
 
+        ### Se lee el encabezado que viene como parte principal del prootocolo para definir como
+        ### se leeran los siguientes parametros que se definieron como parte del protocolo
         opcionJuego = objeto['opcion']
+
+        ### La opcion 1 es para poder iniciar la partida, dado que se debe de poder avisar
+        ### a los demas jugadores que la partida esta lista cuando ya hay al menos 3 jugadores
         if opcionJuego == 1:
             if startGame[port] == False:
+                ### Se revisa que haya al menos 3 jugadores para poder empezar la partida
                 if ROOMS[port] > 2 and ROOMS[port] < 6:
+                    ### Se hace un set de los elementos del juego
+                    ### Se revuelve el mazo
+                    random.shuffle(ROOMpiles[port])
+                    random.shuffle(ROOMpiles[port])
+                    random.shuffle(ROOMpiles[port])
+
+                    ### Se le da un defuse a cada jugador
+                    for player in ROOMScon[port]:
+                        ROOMpilesPlayers[port][player].append(7)
+                        ROOMamountDefuses[port] = ROOMamountDefuses[port] - 1
+                    
+                        ### Se reparten 7 cartas a cada jugador
+                        for k in range(7):
+                            pilaTemporal = ROOMpiles[port]
+                            ROOMpilesPlayers[port][player].append(pilaTemporal.pop())
+                            ROOMpiles[port] = pilaTemporal
+
+                    ### Se agregan los defuses y bombas al mazo
+                    random.shuffle(ROOMpiles[port])
+                    random.shuffle(ROOMpiles[port])
+                    random.shuffle(ROOMpiles[port])
+
+                    for player in ROOMScon[port]:
+                        if i < len(ROOMScon[port]) - 1:
+                            ### Se agrega una bomba y se revuelve
+                            ROOMpiles[port].append(8)
+                            random.shuffle(ROOMpiles[port])
+                        
+                    for j in range(ROOMamountDefuses[port]):
+                        ### Se agrega una bomba y se revuelve
+                        ROOMpiles[port].append(7)
+                        random.shuffle(ROOMpiles[port])
+
+                    ### Hay que revolver bien las cartas :)
+                    random.shuffle(ROOMpiles[port])
+
+                    ### Se notifica al resto de jugadores que inicio el juego
                     mensajeInicio = {
                         'header' : 'inicio'
                     }
@@ -47,6 +117,19 @@ def game(cli_sock, port, username):
                     mensajeInicioP = pickle.dumps(mensajeInicio)
                     for client in ROOMScon[port]:
                         client.send(mensajeInicioP)
+
+                    ### Luego se notifica el player que va de primero
+                    ROOMSturnPlayers[port].append([0,1])
+                    mensajeTurno = {
+                        'header' : 'turno',
+                        'mensaje' : '\nEs el turno del User '+str(ROOMSturnPlayers[port][0][0])+'.'
+                    }
+                    mensajeTurnoP = pickle.dumps(mensajeTurno)
+                    for client in ROOMScon[port]:
+                        client.send(mensajeTurnoP) 
+
+                ### En caso no haya suficientes jugadores, entonces se manda un mensaje de error
+                ### a todos los clientes
                 else:
                     mensajeInicio = {
                         'header' : 'fallo',
@@ -55,7 +138,8 @@ def game(cli_sock, port, username):
                     mensajeInicioP = pickle.dumps(mensajeInicio)
                     for client in ROOMScon[port]:
                         client.send(mensajeInicioP)
-
+            ### En caso se quiera iniciar el juego cuando ya se ha iniciado entonces se manda un mensaje
+            ### al cliente para avisarle que la partida ya esta en curso
             else:
                 mensajeInicio = {
                     'header' : 'fallo',
@@ -64,9 +148,14 @@ def game(cli_sock, port, username):
                 mensajeInicioP = pickle.dumps(mensajeInicio)
                 cli_sock.send(mensajeInicioP)
 
+        ### La opcion 2 es para poder utilizar el Chat (Broadcast) para poder mandar mensajes con el resto de
+        ### jugadores
         elif opcionJuego == 2:
+            ### Solo se pueden mandar mensajes cuando la partida ha iniciado
             if startGame[port] == True:
                 broadcast_usr(username, cli_sock, objeto, port)
+            ### Si no se ha iniciado mandamos un mensaje de error al cliente para indicarle que la partida
+            ### aun no ha iniciado
             else:
                 mensajeInicio = {
                     'header' : 'fallo',
@@ -74,6 +163,32 @@ def game(cli_sock, port, username):
                 }
                 mensajeInicioP = pickle.dumps(mensajeInicio)
                 cli_sock.send(mensajeInicioP)        
+
+        ### La opcion 3 es para poder realizar una jugada cuando es el turno del cliente
+        elif opcionJuego == 3:
+            ### Solo se pueden hacer jugadas cuando la partida ha iniciado
+
+
+            
+            if startGame[port] == True:
+                mensajeInicio = {
+                    'header' : 'fallo',
+                    'mensaje' : '\nTurno jugador: '+str(ROOMSturnPlayers[port])+' Conexiones: '+str(ROOMScon[port])
+                }
+                mensajeInicioP = pickle.dumps(mensajeInicio)
+                cli_sock.send(mensajeInicioP)
+
+
+                #broadcast_usr(username, cli_sock, objeto, port)
+            ### Si no se ha iniciado mandamos un mensaje de error al cliente para indicarle que la partida
+            ### aun no ha iniciado
+            else:
+                mensajeInicio = {
+                    'header' : 'fallo',
+                    'mensaje' : '\nEl juego aun no ha sido.'
+                }
+                mensajeInicioP = pickle.dumps(mensajeInicio)
+                cli_sock.send(mensajeInicioP)    
 
 def broadcast_usr(uname, cli_sock, objeto, port):
     try:
@@ -94,7 +209,9 @@ def b_usr(cs_sock, sen_name, msg, port):
             }
             client.send(pickle.dumps(objeto))
 
-
+### Se solicita que se ingresen los parametros de IP y PORT del servidor
+#HOST = input("Bienvenido a Exploding Kittens Server\nIngrese la IP del host: ")
+#PORT = int(input("\nIngrese el puerto del Server: "))
 c = PORT
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
--- player.py
+++ player.py
@@ -5,10 +5,22 @@ import pickle
 ### Variables a setear para un cliente
 inicioPartida = False
 
+### Variables para el manejo de una partida en una sala
+CARDNO = 1
+CARDATACK = 2
+CARDSKIP = 3
+CARDFAVOR = 4
+CARDSHUFFLE = 5
+CARDSEEFUTURE = 6
+CARDDEFUSE = 7
+CARDBOMB = 8
+
+### Funciones para poder escuchar y enviar mensajes al Server
+## Enviar mensajes
 def send():
     global inicioPartida
     while True:
-        print('\nTienes las siguientes opciones\n1. Iniciar la partida\n2. Enviar Mensaje')
+        print('\nTienes las siguientes opciones\n1. Iniciar la partida\n2. Enviar Mensaje\n3. Retroalimentacion')
         opcion = int(input('\nIngresa el numero de opcion > '))
         if opcion == 1:
             if inicioPartida == False:
@@ -32,6 +44,17 @@ def send():
             else:
                 print('La partida aun no ha sido iniciada.')
 
+        elif opcion == 3:
+            if inicioPartida == True:
+                objeto = {
+                    'opcion' : opcion
+                }
+                data_string = pickle.dumps(objeto)
+                cli_sock.send(data_string)
+            else:
+                print('La partida aun no ha sido iniciada.')        
+
+## Recibir mensajes
 def receive():
     global inicioPartida
     while True:
@@ -55,11 +78,19 @@ def receive():
             print(mensajeFallo)
             print('\n> ')
 
+        if objeto['header'] == 'turno':
+            mensajeTurno = objeto['mensaje']
+            print(mensajeTurno)
+            print('\n> ')
+
 ### Se prepara la conexion por medio de sockets
 cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 ### Nos conectamos al servidor que se encarga de crear las salas
 ip_address = 'localhost'
 port = 50001
+### Se solicita que se ingresen los parametros de IP y PORT del servidor
+#ip_address = input("Bienvenido a Exploding Kittens Server\nIngrese la IP del host: ")
+#port = int(input("\nIngrese el puerto del Server: "))
 ### Conexion al server creador de salas 
 cli_sock.connect((ip_address, port))
 data = cli_sock.recv(4096)
